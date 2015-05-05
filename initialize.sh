afdf5bee5984b352503971c2ba12f0700b0667ff
somebody <somebody@somewhere.com>
2015-05-05 15:02:52 +0000

    removing initialize.sh

--- initialize.sh
+++
@@ -1,9 +0,0 @@
-#change DOMAIN,
-
-#TODO: look up bash variables
-#DOMAIN
-#goes 2 links deep, verbose, ignores robots.txt
-httrack https://www.whatever.com -r2 -v -s0
-
-cd www.whatever.com
-git clone https://github.com/venturecommunism/sites-ui.git
